1343da36b7cf9bad295702be50c73d775b85b5eb
Dima Kacevich <d.katsevich.4@gmail.com>
2024-02-18 14:22:18 +0200

    Add Position model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Position;
+use App\Models\User;
+use DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $positions = Position::factory(5)->create();
+
+
+        $userCount = 50;
+        $userData = [];
+        for ($i = 0; $i < $userCount; $i++) {
+            $userData[] = User::factory()
+                ->makeOne([
+                    'position_id' => $positions->random()->id
+                ])
+                ->toArray();
+        }
+
+
+        DB::table('users')->insert($userData);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
