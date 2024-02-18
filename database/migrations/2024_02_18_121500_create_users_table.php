2815eeadd1cc04b56a7381d3016cf65d82474a18
Dima Kacevich <d.katsevich.4@gmail.com>
2024-02-18 14:22:36 +0200

    Add User model and factory

--- database/migrations/2024_02_18_121500_create_users_table.php
+++ database/migrations/2024_02_18_121500_create_users_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Position;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('phone')->unique();
+
+            $table->string('photo');
+
+            $table->foreignIdFor(Position::class)->index()->constrained();
         });
     }
 
