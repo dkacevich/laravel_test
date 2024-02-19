e20cf108cc6296412bef81a566a30852d87c9dbe
Dima Kacevich <d.katsevich.4@gmail.com>
2024-02-19 12:23:20 +0200

    Update seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use File;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Storage;
+use Str;
 
 /**
  * @extends Factory<User>
@@ -16,13 +19,16 @@ class UserFactory extends Factory
     {
         $phoneBody = sprintf("%09d", mt_rand(0, 999999999));
 
+        $image = fake()->image(format: 'jpg');
+        $imageName = Str::random(30) . '.jpg';
+        Storage::disk('media')->put($imageName, File::get($image));
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'phone' => "+380" . $phoneBody,
 
-            //TODO Fix this
-            'photo' => "test photo url",
+            'photo' => $imageName,
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Position;
+use App\Models\User;
+use DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,30 +16,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-//        $positions = Position::factory(5)->create();
-//
-//
-//        $userCount = 50;
-//        $userData = [];
-//        for ($i = 0; $i < $userCount; $i++) {
-//            $userData[] = User::factory()
-//                ->makeOne([
-//                    'position_id' => $positions->random()->id
-//                ])
-//                ->toArray();
-//        }
-//
-//
-//        DB::table('users')->insert($userData);
+        $positions = Position::factory(5)->create();
 
 
-//        AccessToken::factory(10)->create();
+        $userCount = 50;
+        $userData = [];
+        for ($i = 0; $i < $userCount; $i++) {
+            $userData[] = User::factory()
+                ->makeOne([
+                    'position_id' => $positions->random()->id
+                ])
+                ->toArray();
+        }
 
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert($userData);
     }
 }
